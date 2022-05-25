97fd086f1bcccf50d36636a96c8a6726b180b863
CalvinAH <calvinbusiness24@gmail.com>
2022-05-25 18:24:56 +0800

    push 2

--- src/Server/ComShop_Impl.java
+++ src/Server/ComShop_Impl.java
@@ -4,14 +4,21 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.List;
 
+import javax.crypto.spec.PBEKeySpec;
+
 import Classes.Items;
 import Classes.User;
 import Classes.role_project;
 import Interface.ComShop;
 
 public class ComShop_Impl extends UnicastRemoteObject implements ComShop{
-    role_project roles = new role_project("Customer","Sales Executive","Admin","Supplier");
-    User user1 = new User("1","Calvin", "Hermawan", "1234", roles.getRole3());
+    //role_project roles = new role_project("Admin","Sales Executive","Supplier","Customer");
+    role_project admin = new role_project("Admin");
+    role_project saleE = new role_project("Sales Executive");
+    role_project supp = new role_project("Supplier");
+    role_project customer = new role_project("Customer");
+    User user1 = new User("U1","Calvin", "Hermawan", "1234", admin);
+    User user2 = new User("U2","Osamah", "2", "1234", saleE);
 
     protected ComShop_Impl() throws RemoteException {
         super();
@@ -21,7 +28,7 @@ public class ComShop_Impl extends UnicastRemoteObject implements ComShop{
     @Override
     public boolean userCheck(String userName) throws RemoteException {
         // TODO Auto-generated method stub
-        if (userName == "Calvin Hermawan"){
+        if (userName == user1.getUserName()||userName== user2.getUserName()){
             return true;
         }
         return false;
@@ -30,6 +37,9 @@ public class ComShop_Impl extends UnicastRemoteObject implements ComShop{
     @Override
     public User userlogin(String userName, String passNum) throws RemoteException {
         // TODO Auto-generated method stub
+        if (userName== user1.getUserName()&& passNum == user1.getPassNum()){
+            return user1;
+        }
         return null;
     }
 
